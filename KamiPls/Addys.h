dc27cf2d11cbd98b6b44429db5154f2a9dfe5dff
Waty <wartaarnoutse@gmail.com>
2015-11-18 22:30:37 +0100

    Updated addies to 116.1

--- KamiPls/Addys.h
+++ KamiPls/Addys.h
@@ -2,12 +2,12 @@
 #include <stdint.h>
 
 /**
-* Contains updated adresses for MapleStory v114.1
+* Contains updated adresses for MapleStory v116.1
 * Updated using Waty's Automated AOB scanner
 */
 
 
-const uint32_t CMobPoolPtr = 0x199B290; //TSingleton<CMobPool> aka MobBasePtr
-const uint32_t CDropPoolPtr = 0x199B8D4; //TSingleton<CDropPool>
-const uint16_t CMobTemplateOffset = 0x240; //CMob::m_pTemplate
+const uint32_t CMobPoolPtr = 0x1AA2D14; //TSingleton<CMobPool> aka MobBasePtr
+const uint32_t CDropPoolPtr = 0x1AA3554; //TSingleton<CDropPool>
+const uint16_t CMobTemplateOffset = 0x254; //CMob::m_pTemplate
 const uint16_t TemplateIdOffset = 0xB8; //CTemplate::m_templateId
