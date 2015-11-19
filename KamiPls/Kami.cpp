7adfa26691387c5e0845a7409a5a2e1cbcd36a0d
Waty <wartaarnoutse@gmail.com>
2015-11-19 11:09:44 +0100

    Cleaned up project structure

--- KamiPls/Kami.cpp
+++ KamiPls/Kami.cpp
@@ -8,12 +8,7 @@ void __fastcall update(CUserLocal* ecx);
 
 struct CUserLocalInternal
 {
-	struct IGObjVtbl
-	{
-		uint32_t* Update;
-	};
-
-	IGObjVtbl* vfptr;
+	struct IGObjVtbl { uint32_t* Update; }*vfptr;
 };
 
 void kami::enable(bool state)
@@ -21,17 +16,11 @@ void kami::enable(bool state)
 	auto vfptr = reinterpret_cast<CUserLocalInternal*>(CUserLocal::GetInstance())->vfptr;
 	if (state)
 	{
-		if (original_update == NULL)
-		{
-			original_update = *vfptr->Update;
-			vfptr->Update = reinterpret_cast<uint32_t*>(&update);
-		}
+		if (original_update == NULL) original_update = *vfptr->Update;
+		vfptr->Update = reinterpret_cast<uint32_t*>(&update);
+
 	}
-	else if (original_update != NULL)
-	{
-		vfptr->Update = &original_update;
-		original_update = NULL;
-	}
+	else if (original_update != NULL) vfptr->Update = &original_update;
 }
 
 void teleport(POINT pt)
@@ -47,4 +36,3 @@ void __fastcall update(CUserLocal* ecx)
 
 	reinterpret_cast<void(__thiscall *)(CUserLocal*)>(original_update);
 }
-
